f221f2ee6e7fc836e34ca66b05e219e8954185cd
Bernard <pirlea.bernard@gmail.com>
2020-03-05 01:42:40 +0200

    Updates

---
+++ precache-manifest.eb4a86a4230d1d2a869ef563bec124de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cebc2f6a11408011e188559280d53a",
+    "url": "/bernardpirlea.github.io/index.html"
+  },
+  {
+    "revision": "2831b7296dd540c5b7e0",
+    "url": "/bernardpirlea.github.io/static/css/main.fc550b5e.chunk.css"
+  },
+  {
+    "revision": "facc135afec469259be8",
+    "url": "/bernardpirlea.github.io/static/js/2.c5b799b4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/bernardpirlea.github.io/static/js/2.c5b799b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2831b7296dd540c5b7e0",
+    "url": "/bernardpirlea.github.io/static/js/main.125cde7d.chunk.js"
+  },
+  {
+    "revision": "4282d07f231b2c2dcafb",
+    "url": "/bernardpirlea.github.io/static/js/runtime-main.f00fdf93.js"
+  }
+]);
