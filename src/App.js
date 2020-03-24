ead66bd27d862da423bddd2a3c05db867fcf352a
Bernard <pirlea.bernard@gmail.com>
2020-03-24 22:40:22 +0200

    projects page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Academic from "./components/Academic";
 import Progress from "./components/Progress";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Route exact path="/academic" component={Academic} />
-        <Route exact path="/projects" component={Progress} />
+        <Route exact path="/projects" component={Projects} />
         <Route exact path="/blog" component={Progress} />
         <Route exact path="/portfolio" component={Main} />
       </div>
---
+++ src/components/Projects.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import simpleApiImage from "../images/simpleApi.png";
+import portfolioImage from "../images/portfolio.png";
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div className="container" style={divStyle}>
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card medium">
+              <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={simpleApiImage} />
+              </div>
+              <div className="card-content">
+                <span className="card-title">Simple Store Application</span>
+                <p>
+                  An online store. Build using EJS on the Frontend, Node.js and
+                  MongoDB on the Backend
+                </p>
+              </div>
+              <div className="card-action">
+                <a
+                  href="https://github.com/bernardpirlea/Work"
+                  className="black-text"
+                >
+                  Source Code
+                </a>
+                <a
+                  href="https://salty-falls-47840.herokuapp.com/"
+                  className="black-text"
+                >
+                  Application
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m6">
+            <div className="card medium">
+              <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={portfolioImage} />
+              </div>
+              <div className="card-content">
+                <span className="card-title">Portfolio Website</span>
+                <p>My Portfolio Website build using React.js and Materialize</p>
+              </div>
+              <div className="card-action">
+                <a
+                  href="https://github.com/bernardpirlea/portfolio"
+                  className="black-text"
+                >
+                  Source Code
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const divStyle = {
+  paddingTop: "60px"
+};
